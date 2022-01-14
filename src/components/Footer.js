03b1a857c1676bd8b3d5de9db5728997498112ca
Joel Munoz <j.munoz1@yahoo.com>
2022-01-14 13:04:15 -0800

    footer edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { BsLinkedin, BsGithub, BsInstagram } from 'react-icons/bs';
 
-export default function Footer() {
-  return <h1>in the Footer</h1>;
+const Footer = () => {
+  const date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container id="Footer" fluid>
+      <Row>
+        <Col>
+          <p >
+            ©{year} Joel Munoz
+          </p>
+        </Col>
+        <Col>
+          <p>Front-End Developer</p>
+        </Col>
+        <Col>
+          <a href="https://www.linkedin.com/in/moonoz/" >
+            <BsLinkedin />
+          </a>
+          <BsGithub />
+          <BsInstagram />
+
+        </Col>
+      </Row>
+    </Container>
+  )
 }
+export default Footer;
