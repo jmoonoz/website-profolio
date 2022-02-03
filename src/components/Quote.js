d1cff3892cc9b5bfa30f31a3482cd69d04d48d99
Joel Munoz <j.munoz1@yahoo.com>
2022-02-02 22:21:12 -0800

    creating function to change text

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,43 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "reactstrap";
 import { FaCode } from "react-icons/fa";
 import { GoCode } from "react-icons/go";
 import QuoteArray from './quotes';
 
-// console.log(QuoteArray.length);
-// let intervalDuration = 2000,
-//   currentIndex = 0,
-//   arrayLength = QuoteArray.length;
 
-class Quote extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      key: 0,
-      quote: "",
-      speaker: ""
+
+const Quote = () => {
+  const [index, setIndex] = useState(0);
+
+  const quoteArray = [
+    {
+      id: 0,
+      quote: "Craft is what we are expected to know; art is the unexpected use of our craft",
+      auth: "- Ed Catmull"
+    },
+    {
+      id: 1,
+      quote: "I guess one person can make a difference",
+      auth: "- Stan Lee"
     }
-  }
+  ];
+  const q = "";
+  const a = "";
 
-
-  // setInterval(() => {
-  //   if (currentIndex === arrayLength) {
-  //     currentIndex = 0;
-  //   } else {
-  //     this.setState({
-  //       key: QuoteArray[currentIndex].key,
-  //       quote: QuoteArray[currentIndex].quote,
-  //       speaker: QuoteArray[currentIndex].auth
+  // function textChange() {
+  //   let i = 0;
+  //   setInterval(() => {
+  //     $('.quote-text').fadeOut(function () {
+  //       $(this).html(quoteArray[i = (i + 1) % quoteArray.length].quote).fadeIn();
   //     });
-  //     currentIndex++;
-  //   }
+  //     $('.quote-text').fadeOut(function () {
+  //       $(this).html(quoteArray[i = (i + 1) % quoteArray.length].auth).fadeIn();
+  //     });
 
-  // }, intervalDuration);
+  //   }, 3000)
 
-// componentDidMount() {
+  // }
+  // textChange();
 
-// }
-render() {
+
+//  setInterval(() => {
+//       if (index < quoteArray.length-1) {
+//         setIndex(index + 1);
+//       } else {
+//         setIndex(0);
+//       }
+//     }, 3000);
+
+
   return (
     <Container id="Quote" fluid>
       <Container>
@@ -45,17 +56,19 @@ render() {
 
           <Col sm={12}>
             <h3 className="quote-text" >
-              Craft is what we are expected to know; art is the unexpected use of our craft
+              {quoteArray[index].quote}
+              {/* Craft is what we are expected to know; art is the unexpected use of our craft */}
               {/* {this.state.quote} */}
             </h3>
           </Col>
           <Col sm={12}>
-            <p className="quote-auth">- Ed Catmull</p>
+
+            <p className="quote-auth">{quoteArray[index].auth}</p>
           </Col>
         </Row>
       </Container>
     </Container>
   );
-}
+
 };
 export default Quote;
