f5ac6214fcfb0338d72d2858ccc29b1bc7914c65
Joel Munoz <j.munoz1@yahoo.com>
2022-02-09 22:59:01 -0800

    nav bar align to the right

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,24 @@
 import React from "react";
 import "../style/styles.scss";
-import { Navbar,Nav,NavDropdown, Container } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 
 const NavBar = () => {
   return (
     <Navbar id='nav' fixed="top" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Joel Munoz</Navbar.Brand>
+        <Navbar.Brand href="#Banner">Joel Munoz</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Container>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#Quote">Quote</Nav.Link>
+              <Nav.Link href="#About">About</Nav.Link>
+              <Nav.Link href="#Proects">Projects</Nav.Link>
+              <Nav.Link href="#Skills">Skills</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Container>
     </Navbar>
   );
